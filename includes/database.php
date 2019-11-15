6994d0eae497c718d4b9e2219d8e9b7b3b0092ea
Enoch NAYIMULI <enayimul@c4r9s3.wethinkcode.co.za>
2019-11-15 15:49:22 +0200

    good

--- includes/database.php
+++ includes/database.php
@@ -2,4 +2,21 @@
 $servername = "localhost";
 $db_username = "root";
 $db_password = "12233221Ee";
+
+try {
+    $db = new PDO("mysql:host=$servername", $db_username, $db_password);
+    // set the PDO error mode to exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //$sql = "CREATE DATABASE IF NOT EXISTS camagru";
+    // use exec() because no results are returned
+    $db->exec("USE camagru");
+   // echo "Database created successfully <br>";
+    }
+    catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+
+
 ?>
